d054d36d1d865c24bbd358a7c08d755d001c785f
Kevin <kevinsnandu@yahoo.com>
2018-07-23 00:27:40 +0530

    modified punc

--- punctuations.py
+++ punctuations.py
@@ -1,10 +1,14 @@
-a_string=input("Enter a string with punctuations: ")
-punctuations=list('''!()-[]{};:'"\,<>./?@#$%^&*_~''')
-print(punctuations)
-length=(len(punctuations))
-for i in range(0, length):
-  if punctuations[i] in a_string:
-    final_string=a_string.replace(punctuations[i], "")
-    a_string=final_string
-  i+=1  
-print(final_string)
+def setpunc():
+  a_string=input("Enter a string with punctuations: ")
+  punctuations=list('''!()-[]{};:'"\,<>./?@#$%^&*_~''')
+  length=(len(punctuations))
+  for i in range(0, length):
+    if punctuations[i] in a_string:
+      final_string=a_string.replace(punctuations[i], "")
+      a_string=final_string
+    else:
+      final_string=a_string
+    i+=1  
+  print(final_string)
+if __name__=="__main__":
+  setpunc()
